10b16497cd796b717b7aa15719ba400a645f7319
RudraBanerjee <rudrabanerjee2002@gmail.com>
2025-02-22 11:55:07 +0530

    Final Model Creation

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,16 @@
- print(point)
+def group_texts_by_label(data):
+#     if isinstance(data, str):
+#         data = json.loads(data)
+    
+#     grouped_data = {}
+#     for item in data:
+#         label = item.get('label')
+#         text = item.get('text')
+
+#         # Initialize an array for the label if it doesn't exist
+#         if label not in grouped_data:
+#             grouped_data[label] = []
+#         # Append the text to the label's array
+#         grouped_data[label].append(text)
+
+#     return grouped_data
